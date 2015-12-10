41bf71f966dd484f128f4f5170b61d17baecc177
lolicon <lolicon@mail.az>
2015-12-10 15:54:09 +0800

    almost done

--- captcha/spider.py
+++ captcha/spider.py
@@ -6,104 +6,212 @@ import PIL
 import pyocr
 from PIL import Image
 import StringIO
+import traceback
+import re
+import operator
+import tempfile 
+import ipdb
+import time
 
 """
-	take cookie from GLOBAL object,
-	when expired, replace or retrieve & save the GLOBAL session,
+  take cookie from GLOBAL object,
+  when expired, replace or retrieve & save the GLOBAL session,
 
 steps:
-	1. detect cookie,
-	2. ocr and regain cookie (captcha and login)
-	3. start working threads 
+  1. detect cookie,
+  2. ocr and regain cookie (captcha and login)
+  3. list available orders
+  4. submit orders
 """
 
 
+"""
+LIST:
+https://www.bmfinn.com//coin/buyData?pageNo=_pageNo_&timestamp=_timestamp_
+<input value="9931" name="sellEpRa" type="radio">
 
+BUY:
+https://www.bmfinn.com/coin/buy?timestap=1449652284167".
+timestap:1449652167397
+orderId:9931
+thpassword:888888
+"""
 
 CAPTCHA_URL = "https://www.bmfinn.com/catcha/captchaImage"
+LOGIN_URL = "https://www.bmfinn.com/index/validate?timestap="
+ORDER_LIST_URL = "https://www.bmfinn.com//coin/buyData"
+SUBMIT_ORDER = "https://www.bmfinn.com/coin/buy"
+
+USERNAME = 'dzm888'
+PASSWORD = '888888'
+PAY_PASSWORD = '888888'
+
 PROXIES = {'http':'http://127.0.0.1:1090','https':'http://127.0.0.1:1090'}
 
 def dump(*args):
-	[print(arg) for arg in args]
+  [print(arg) for arg in args]
 
 
 class OCR:
-	def __init__(self,image):
-		self.image = image
-		self.width,self.height = image.size
-		self.pix = image.load()
-		self.remain_blur_times = 10
-		self.ocr = pyocr.get_available_tools()[0]
+  def __init__(self,image):
+    self.image = image
+    self.width,self.height = image.size
+    self.pix = image.load()
+    self.remain_blur_times = 10
+    self.ocr = pyocr.get_available_tools()[0]
 
-	def blur():
-		[self.__blur_pixel(x,y) for x in xrange(1,self.width-1) for y in xrange(1,self.height-1) if reduce(operator.add,self.pix[(x,y)],0) < 500]
+  def blur(self):
+    [self.__blur_pixel(x,y) for x in xrange(1,self.width-1) for y in xrange(1,self.height-1) if reduce(operator.add,self.pix[(x,y)],0) < 500]
 
-	def __blur_pixel(x,y):
-		top = self.pix[(x,y+1)]
-		bottom = self.pix[(x,y-1)]
-		self.pix[(x,y)] = tuple([(top[i] + bottom[i]) / 2 for i in (0,1,2)])
-		
-	def identify_image(self,remain_blur_times = 10):
-		res = self.ocr.image_to_string(self.image,builder=pyocr.tesseract.DigitBuilder())
-		dump("recongnize as : " + res)
-		if len(res) == 4 and re.match(ur'^\d+$',res):
-			return res
-		else:
-			if remain_blur_times <= 0:
-				raise Exception("can not recongnize image, change another") 
-			self.blur()
-			self.run(remain_blur_times-1)
+  def __blur_pixel(self,x,y):
+    top = self.pix[(x,y+1)]
+    bottom = self.pix[(x,y-1)]
+    self.pix[(x,y)] = tuple([(top[i] + bottom[i]) / 2 for i in (0,1,2)])
+    
+  def identify_image(self,remain_blur_times = 10):
+    res = self.ocr.image_to_string(self.image,builder=pyocr.tesseract.DigitBuilder())
+    dump("recongnize as : " + res)
+    if len(res) == 4 and re.match(ur'^\d+$',res):
+      return res
+    else:
+      if remain_blur_times <= 0:
+        raise Exception("can not recongnize image, change another") 
+      self.blur()
+      return self.identify_image(remain_blur_times-1)
 
 
 class User:
-	RunningUsers = []
-	def __init__(self):
-		self.session = Session()
-		self.RunningUsers.append(self)
+  RunningUsers = []
+  def __init__(self):
+    self.session = Session()
+    self.RunningUsers.append(self)
 
-	def trim(self):
-		self.RunningUsers.remove(self)
+  def trim(self):
+    self.RunningUsers.remove(self)
 
-	def read_captcha(self,retry_count = 5):
-		try:
-			dump('read_caption',retry_count)
-			self.image_response = self.session.get(CAPTCHA_URL,verify=False,proxies=PROXIES)
-			dump('read_caption_done')
-			image = Image.open(StringIO.StringIO(self.image_response.content))
-			self.ocr = OCR(image)
-			return self.identify_image()
-		except Exception,e:
-			dump(e)
-			if retry_count >= 1:
-				self.read_captcha(retry_count - 1)
-			else:
-				dump('failed get captcha')
+  def read_captcha(self,retry_count = 5):
+    dump('***read_captcha',retry_count)
+    try:
+      self.image_response = self.session.get(CAPTCHA_URL,verify=False,proxies=PROXIES)
+      dump('read_captcha_done')
+      image = Image.open(StringIO.StringIO(self.image_response.content))
+      filename = re.sub(ur'^.*/','',tempfile.mktemp())
+      image.save(filename,"JPEG")
+      self.ocr = OCR(image)
+      return self.ocr.identify_image()
+    except Exception,e:
+      dump("captcha err:",traceback.format_exc())
+      if retry_count > 0:
+        return self.read_captcha(retry_count - 1)
+      else:
+        dump('failed get captcha')
+        return ""
 
-	def cookie(self):
-		return str(self.session.cookies)
+  def cookie(self):
+    return str(self.session.cookies)
 
-	def login(self):
-		pass
+  def login(self,code,retry_count=30):
+    dump('***login',retry_count)
+    login_url = LOGIN_URL + str(int(time.time() * 1000))
+    try:
+      resp = self.session.post(login_url,data={
+          'userName':USERNAME,
+          'password':PASSWORD,
+          'code':code
+        })
+      dump("login_resp",resp.content)
+      if(re.search(u'',resp.content)):
+        return (True,resp.content)
+      else:
+        return (False,resp.content)
+    except Exception ,e:
+      dump("login err :" + str(retry_count) + ":",traceback.format_exc())
+      if retry_count > 0:
+        return self.login(code,retry_count-1)
+      else:
+        return (False,str(e))
 
-	def other_action(self):
-		pass
+  def list_available(self,retry_count=100):
+    dump('***list_available',retry_count)
+    try:
+      resp = self.session.get(ORDER_LIST_URL,data={
+        "pageNo":1,
+        "timestamp":str(int(time.time()*1000))
+      },allow_redirects=False)
+      dump("** list_available",resp.content)
+      if resp.status_code == 302: # login again
+        return (False,302)
+      elif resp.content.find("sellEpRa") != -1: # buy items
+        # <input value="9931" name="sellEpRa" type="radio">
+        pattern = ur'value="(\d+)"\s+name="sellEpRa"'
+        orders = re.findall(pattern,resp.content)
+        return (True,orders) if len(orders)  > 0 else (False,0)
+      else: # refresh again
+        pass
+    except Exception,e:
+        dump("**list_available",e)
 
-	@classmethod
-	def current_running(clz):
-		return len(clz.RunningUsers)
+    if retry_count > 0:
+      return self.list_available(retry_count-1)
+    else:
+      dump("**list_available refresh over")
+      return (False,1)
 
-	@classmethod
-	def clear_session(clz):
-		[user.shutdown for user in clz.RunningUsers]
+
+  def submit_order(self,order_id,submit_count=100):
+    dump("***submit_order",order_id,submit_count)
+    try:
+      resp = self.session.get(SUBMIT_ORDER,data={
+        'timestamp':str(int(time.time()*1000)),
+        'orderId':str(order_id),
+        'thpassword':PAY_PASSWORD
+      },allow_redirects=False)
+      dump("**submit_order",resp.content)
+      code = resp.status_code
+      if code == 302:
+        dump("submit order need login, lets try again",resp.status_code)
+        return False
+      else: # submit 30 times
+        return self.submit_order(order_id,submit_count-1)
+    except Exception,e:
+      dump("**submit_order  err",e)
+      if submit_count < 0:
+        return False
+      else:
+        return self.submit_order(order_id,submit_count-1)
+
+
+
+  @classmethod
+  def current_running(clz):
+    return len(clz.RunningUsers)
+
+  @classmethod
+  def clear_session(clz):
+    [user.shutdown() for user in clz.RunningUsers]
 
 
 def main():
-	user  = User()
-	captcha = user.read_captcha();
-	dump("captcha : " + str(captcha))
+  user  = User()
+  captcha = user.read_captcha();
+  dump("captcha : " + str(captcha))
+  dump("cookie : " + user.cookie())
+  if len(captcha) > 0:
+    logined,login_resp = user.login(captcha)
+    if logined:
+      res,reason = user.list_available()
+      if res:
+        [user.submit_order(order_id) for order_id in reason]
+      else:
+        dump("list failed :" + str(reason))
+    else:
+      dump("login failed: ",login_resp)
+  else:
+    dump("captcha failed:")
+
 
 if __name__ == '__main__':
-	main()
+  main()
 
 
 
